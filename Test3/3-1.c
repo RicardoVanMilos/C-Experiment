dfbbb2b91001df5843fe9e5020da38b2f5aaaad4
RicardoVanMilos <vandarkholmes8898@gmail.com>
2020-10-20 20:56:57 +0800

    Add 3-1

---
+++ Test3/3-1.c
@@ -0,0 +1,28 @@
+/*实验3-1改错题程序：计算s=1!+2!+3!+…+n!*/
+#include <stdio.h>
+
+long long sum_fac(int n);
+
+int main(void)
+{
+    int k;
+    long long sum;
+    for (k = 1; k <= 20; k++)
+    {
+        sum = sum_fac(k);
+        printf("k=%d\tthe sum is %lld\n", k, sum);
+    }
+    return 0;
+}
+
+long long sum_fac(int n)
+{
+    long long s = 0, fac = 1;
+    int i;
+    for (i = 1; i <= n; i++)
+    {
+        fac *= i;
+        s += fac;
+    }
+    return s;
+}
